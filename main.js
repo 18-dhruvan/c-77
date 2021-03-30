6d57cbde3093fc2ed20344dd24d722dca602fc31
18-dhruvan <81638051+18-dhruvan@users.noreply.github.com>
2021-03-30 21:04:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("I am Dhruvan ");
+var firstname ="Dhruvan ";
+console.log(firstname);
+var lastname = "Karthik";
+console.log(lastname);
+var fullname = firstname.concat(lastname);
+console.log(fullname);
+var length_of_fullname= fullname.length;
+console.log(length_of_fullname);
+var uppercase_letters=fullname.toUpperCase();
+console.log(uppercase_letters)
+var lowercase_letters=uppercase_letters.toLowerCase();
+console.log(lowercase_letters)
+var N1= 18 ;
+console.log(N1)
+var N2 = 7 
+console.log(N2)
+console.log(N1+N2)
+console.log(N1*N2)
